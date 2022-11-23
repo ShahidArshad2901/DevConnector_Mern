de096d672d11d19baadfad6373d66336d01f946a
ShahidArshad2901 <shahid.arshad@devsinc.com>
2022-11-23 06:34:17 -0800

    HAndle user registeration in user.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const bcryptjs = require("bcryptjs");
 
 // @route   POST api/users
 // @desc    Register User
@@ -15,12 +19,52 @@ router.post(
       "Please enter a password with 6 or more character"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if User exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User Already exists " }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt the password
+      const salt = await bcrypt.genSalt(10); // this will create salt
+
+      user.password = await bcrypt.hash(password, salt); // this will create a hash
+
+      await user.save(); // used await because user.ssave returns promise
+
+      // Return JSON web token
+
+      res.send("User Registered");
+    } catch (error) {
+      console.log(error.message, "Error");
+      res.status(500).send("Server error");
+    }
   }
 );
 
