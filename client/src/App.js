82fc06b18a98b9650e0ca573d8d5e980ef5f651e
ShahidArshad2901 <shahid.arshad@devsinc.com>
2022-11-28 07:30:32 -0800

    Load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,44 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/layout/Landing";
 import Register from "./components/layout/auth/Register";
 import Login from "./components/layout/auth/Login";
 import Alert from "./components/layout/Alert";
+import "./App.css";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-import "./App.css";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        {/* <Landing /> */}
-        <Navbar />
-        <section className="container">
-          <Alert />
-          <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
-          </Routes>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          {/* <Landing /> */}
+          <Navbar />
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route exact path="/" element={<Landing />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,34 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
 import { setAlert } from "./alert";
 
-import { REGISTER_FAIL, REGISTER_SUCCESS } from "./types";
+import {
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
 
+// LOAD USER
+
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    console.log(res);
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 // Register User
-
 export const register =
   ({ name, email, password }) =>
   async (dispatch) => {
