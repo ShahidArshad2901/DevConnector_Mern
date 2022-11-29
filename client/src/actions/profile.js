08d0ae6cf742f5313b77e4297d35bb0f78ed12f9
ShahidArshad2901 <shahid.arshad@devsinc.com>
2022-11-29 08:53:23 -0800

    Add Create Profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { Navigate } from "react-router-dom";
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -22,3 +23,51 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or update Profile
+
+export const createProfile =
+  (formData, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      // const navigation = useNavigation();
+      const res = await axios.post("/api/profile", formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(edit ? "Profile Updated" : "Profile Created", "success")
+      );
+
+      if (!edit) {
+        console.log("We are here to create");
+        return <Navigate to="/dashboard" />;
+        // navigation.navigate("/dashboard");
+      }
+    } catch (error) {
+      const errors = error.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => {
+          dispatch(setAlert(error.msg, "danger"));
+        });
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusTest,
+          status: error.response.status,
+        },
+      });
+    }
+  };
--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile }) => {
   const [formData, setFormData] = useState({
     company: "",
     website: "",
@@ -37,6 +40,11 @@ const CreateProfile = (props) => {
 
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData);
+  };
   return (
     <section className="container">
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -45,7 +53,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -211,6 +219,8 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createProfile })(CreateProfile);
