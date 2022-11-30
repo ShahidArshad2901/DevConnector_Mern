872c323e7a4ec820007eed1b7cbb489c5a46edbb
ShahidArshad2901 <shahid.arshad@devsinc.com>
2022-11-30 06:57:41 -0800

    Complete Profile actions and reducers, and types

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,8 +4,10 @@ import {
   ACCOUNT_DELETED,
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
+  GET_REPOS,
 } from "./types";
 
 // Get current users profile
@@ -29,8 +31,78 @@ export const getCurrentProfile = () => async (dispatch) => {
   }
 };
 
-// Create or update Profile
+// Get All Profile
+export const getProfiles = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
 
+  try {
+    const res = await axios.get("/api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.log("We are having error");
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Get Profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.log("We are having error");
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Get GitHUb Repos
+export const getGithubRepos = (username) => async (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
+
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.log("We are having error");
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Create or update Profile
 export const createProfile =
   (formData, edit = false) =>
   async (dispatch) => {
